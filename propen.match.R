01a991cb63e0a46f9a6f2d5627e6fb9d51a75809
Jeff Chen <jchen85@gmail.com>
2017-02-16 23:45:43 -0500

    Removed counter

--- propen.match.R
+++ propen.match.R
@@ -16,7 +16,6 @@ kmatch <- function(y1, y2, n = 3, replacement = F){
   y1 <- na.omit(y1)
   y2 <- na.omit(y2)
   for(i in 1:nrow(y1)){
-    print(i)
     ref <- y1[i,2]
     y3 <- rank(abs(y2[,2] - ref), ties.method = "random")
     pos <- y2[match(seq(1,n,1), y3),]
