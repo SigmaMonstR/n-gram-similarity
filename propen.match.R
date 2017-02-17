0f748984505b8a169ac210d398fee126e04bd14e
Jeff Chen <jchen85@gmail.com>
2017-02-16 23:18:46 -0500

    v2 of method
    With documentation

--- propen.match.R
+++ propen.match.R
@@ -1,17 +1,29 @@
 
 kmatch <- function(y1, y2, n = 3, replacement = F){
+  # Produces nearest neighbor propensity score match from two dataframes
+  # Input dataframes should be organized as follows: ID var in column 1, propensity score in column 2
+  #
+  # Args:
+  #       y1: left hand dataframe
+  #       y2: right hand dataframe
+  #       n: number of matches
+  #       replacement: draw with replacement
+  #
+  # Returns:
+  #       a data frame with matches and scores
+  #
   master <- data.frame()
   y1 <- na.omit(y1)
   y2 <- na.omit(y2)
-  for(i in 1:length(y1)){
+  for(i in 1:nrow(y1)){
     print(i)
-    ref <- y1[i]
-    y3 <- rank(abs(y2 - ref), ties.method = "random")
-    pos <- y2[match(seq(1,n,1), y3)]
-    if(length(y2) > n){
-      master <- rbind(master, data.frame(ref = ref, match.val = pos))
+    ref <- y1[i,2]
+    y3 <- rank(abs(y2[,2] - ref), ties.method = "random")
+    pos <- y2[match(seq(1,n,1), y3),]
+    if(nrow(y2) > n){
+      master <- rbind(master, data.frame(ref = ref, ref.org = y1[i, 1], match.val = pos[,2], match.org = pos[,1]))
       if(replacement == F){
-        y2 <- y2[!(y2 %in% pos)]
+        y2 <- y2[!(y2[,1] %in% pos[,1]),]
       }
     }
   }
