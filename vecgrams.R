4f488483c0bb5b1df3fd1d36d9a8306afcaba59a
Jeff Chen <jchen85@gmail.com>
2017-04-07 12:35:47 -0400

    Revised parsing of trigrams

--- vecgrams.R
+++ vecgrams.R
@@ -1,12 +1,10 @@
 
-vecgrams <- function(vec, i, k){
+vecgrams <- function(vec){
   # A function to convert a vector into a series of n-grams.
   # Creates n-grams for lengths i to k
   #
   # Args:
   #       vec: a vector of strings
-  #       i: minimum n-grams
-  #       k: maximum n-grams
   #
   # Returns:
   #       a list with nested lists
@@ -18,14 +16,20 @@ vecgrams <- function(vec, i, k){
     print(row)
     t1 <- unlist(out[row])
     t1 <- t1[t1!="" & nchar(t1)>0]
-    t2 <- c()
-    for(gram in i:k){
-      for(s in 1:length(t1)){
-        if(s-gram + 1 >0){
-          t2 <- c(t2, paste(t1[(s-gram+1):s], collapse = " "))
-        }
-      }
+    
+    if(length(t1)==1){
+      t2 <- t1
+    } else if(length(t1)==2){
+      t01 <- t1
+      t02 <- paste(t1[1:(length(t1)-1)], t1[2:(length(t1))])
+      t2 <- c(t01, t02)
+    } else if(length(t1)>2){
+      t01 <- t1
+      t02 <- paste(t1[1:(length(t1)-1)], t1[2:(length(t1))])
+      t03 <- paste(t1[1:(length(t1)-2)], t1[2:(length(t1)-1)], t1[3:(length(t1))])
+      t2 <- c(t01, t02, t03)
     }
+   
     master <- rbind(master, data.frame(rec = row,  grams = t2))
   }
   
@@ -34,5 +38,5 @@ vecgrams <- function(vec, i, k){
 }
 
 #Example
- # test <- c("Four score and seven years ago","where is star?", "what?")
- # vecgrams(test,1, 5)
+  test <- c("Four score and seven years ago","where is star?", "what?")
+  vecgrams(test)
